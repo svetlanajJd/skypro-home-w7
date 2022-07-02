f74c183cc838766fc1808325081f99b2fec4c84e
sveta <svetlana.fedoseeva.88@mail.ru>
2022-07-02 23:08:12 +0300

    первый коммит

---
+++ src/main/java/pro/sky/skyprohomew7/SkyproHomeW7Application.java
@@ -0,0 +1,14 @@
+package pro.sky.skyprohomew7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproHomeW7Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SkyproHomeW7Application.class, args);
+	}
+
+}
