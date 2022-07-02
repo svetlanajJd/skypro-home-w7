002adefe99ed6efed52a6f6749b4dac17ce2e4b1
sveta <svetlana.fedoseeva.88@mail.ru>
2022-07-03 00:30:12 +0300

    выполнено дз

---
+++ src/main/java/pro/sky/skyprohomew7/EmployeeController.java
@@ -0,0 +1,44 @@
+package pro.sky.skyprohomew7;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/employee/")
+
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/add")
+    public Employee addEmployee(@RequestParam("firstName") String firstName,
+                                @RequestParam("lastName") String lastName) {
+        return employeeService.addEmployee(firstName, lastName);
+    }
+
+    @GetMapping("/find")
+    public Employee findEmployee(@RequestParam("firstName") String firstName,
+                                 @RequestParam("lastName") String lastName) {
+        return employeeService.findEmployee(firstName, lastName);
+    }
+
+    @GetMapping("/remove")
+    public Employee removeEmployee(@RequestParam("firstName") String firstName,
+                                   @RequestParam("lastName") String lastName) {
+        return employeeService.removeEmployee(firstName, lastName);
+    }
+
+    @GetMapping("/total")
+    public Map<Employee, Integer> printTotal() {
+        return employeeService.printTotal();
+    }
+}
---
+++ src/main/java/pro/sky/skyprohomew7/EmployeeNotFoundException.java
@@ -0,0 +1,8 @@
+package pro.sky.skyprohomew7;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class EmployeeNotFoundException extends RuntimeException {
+}
